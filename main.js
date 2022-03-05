ad0aa34aad5bc96c7f94efb3b78d4b5b16d4bae8
rssabbir <sabbirhossainrahad@gmail.com>
2022-03-06 00:00:12 +0600

    almost done nedd few more work with DOM

---
+++ main.js
@@ -0,0 +1,25 @@
+const itemName = document.getElementById("item-name");
+const itemDate = document.getElementById("item-date");
+const itemAmount = document.getElementById("item-amount");
+const addItemBtn = document.getElementById("add-item-btn");
+
+const tableBody = document.getElementById("table-body");
+
+addItemBtn.addEventListener("click", () => {
+  const tableData = [itemName.value, itemDate.value, itemAmount.value];
+  const tr = document.createElement("tr");
+  if (tableData[0].length < 1 || tableData[1].length < 1 || tableData[1].length < 1) {
+    console.log("object");
+  } else {
+    for (let i = 0; i < tableData.length; i++) {
+      const element = tableData[i];
+      const td = document.createElement("td");
+      td.innerText = element;
+      tr.appendChild(td);
+      tableBody.appendChild(tr);
+      document.getElementById("table-caption").style.display = "none";
+    }
+  }
+
+  
+});
